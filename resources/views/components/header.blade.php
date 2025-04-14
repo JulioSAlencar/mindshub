8301f431a47f0c7208a16f2753464df045e9f9c5
Ícaro Moreira <127455211+IcaroMoreir4@users.noreply.github.com>
2025-04-14 20:20:03 -0300

    feat(header): ajusta header para suportar novo layout de disciplinas

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
-<header class="relative bg-white h-12 flex items-center justify-end p-2.5 border-b -z-10">
+<header class="relative bg-white h-12 flex items-center justify-end p-2.5 border-b">
 
-    <!-- @auth
+  <!-- @auth
         @can('is-student')
             <p class="font-semibold mr-10">Aluno: {{ Auth::user()->name }}</p>
         @elsecan('is-teacher')
@@ -12,16 +12,16 @@
         @endcan
     @endauth -->
 
-    <!-- enquanto a autenticação está desligada -->
-    @auth
-    @if (Auth::user()->role === 'student')
-        <p class="font-semibold mr-10">Aluno: {{ Auth::user()->name }}</p>
-    @else (Auth::user()->role === 'teacher')
-        <p class="font-semibold mr-10">Professor: {{ Auth::user()->name }}</p>
-    @endif
-    @else
-        <p class="font-semibold mr-10">Você não está logado</p>
-    @endauth
+  <!-- enquanto a autenticação está desligada -->
+  @auth
+  @if (Auth::user()->role === 'student')
+  <p class="font-semibold mr-10">Aluno: {{ Auth::user()->name }}</p>
+  @else (Auth::user()->role === 'teacher')
+  <p class="font-semibold mr-10">Professor: {{ Auth::user()->name }}</p>
+  @endif
+  @else
+  <p class="font-semibold mr-10">Você não está logado</p>
+  @endauth
 
 
   <div class="flex items-center gap-3 text-2xl">
