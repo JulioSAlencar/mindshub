582f2e930e596990f07b0639eb30ea4ffa8e4c91
Ícaro Moreira <127455211+IcaroMoreir4@users.noreply.github.com>
2025-05-03 19:16:13 -0300

    fix(ui): change input text color from white to black for better readability on gray background

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,9 +29,9 @@
 
       <div class="grid gap-6 w-full">
         <input type="email" name="email" placeholder="Email" value="{{ old('email') }}"
-          class="bg-gray-200 rounded-lg text-lg border-none p-3 text-white">
+          class="bg-gray-200 rounded-lg text-lg border-none p-3 text-black">
         <input type="password" name="password" placeholder="Senha"
-          class="bg-gray-200 rounded-lg text-lg border-none p-3 text-white">
+          class="bg-gray-200 rounded-lg text-lg border-none p-3 text-black">
       </div>
       <!-- Remember Me -->
       <div class="block mt-4">
