d5928432e92b51af272f9c3f65f5bb516a69943d
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-06 13:05:18 -0300

    feat: adds model MissionFeedback

---
+++ app/Models/MissionFeedback.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MissionFeedback extends Model
+{
+    protected $fillable = ['user_id', 'mission_id', 'content'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mission() {
+        return $this->belongsTo(Mission::class);
+    }
+}
+
