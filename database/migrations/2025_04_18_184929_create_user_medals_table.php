edf01d25e39f5251651511f71b90d1ef23439ce5
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-06 15:54:44 -0300

    feat: adds earned_at in user_medals table

--- database/migrations/2025_04_18_184929_create_user_medals_table.php
+++ database/migrations/2025_04_18_184929_create_user_medals_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('medal_id')->constrained('medals')->onDelete('cascade');
+            $table->timestamp('earned_at')->nullable();
             $table->timestamps();
         });
     }
