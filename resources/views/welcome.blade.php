6619a1ec7828b98ef6de0a5e265f91686b3b9398
IcaroMoreir4 <icaromoreira90@gmail.com>
2025-05-13 15:07:35 -0300

    Estiliza tela de boas-vindas com background e logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,45 +2,50 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Mindshub</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('title', 'Mindshub')</title>
+    @vite('resources/css/app.css')
+    @vite(['resources/js/app.js'])
+
+    <style>
+        .bg-custom {
+            background-image: url('{{ asset('assets/images/bgLadingPage.jpg') }}');
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+    
 </head>
 
-<style>
-  .bg-custom {
-    background-image: url('./bgLadingPage.jpg');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-</style>
+<body class="bg-custom h-screen flex flex-col">
 
+    <div class="relative flex flex-1">
+        <!-- Sidebar -->
+        <x-sidebar />
 
-<body
-  style="background-image: url('bgLadingPage.jpg'); background-size: cover; background-position: center;"
-  class="h-screen flex flex-col">
+        <div id="main-header" class="flex flex-col flex-1 ml-20">
+            <x-header />
 
-  <div class="flex justify-between p-8 items-center">
-    <img src="./logoLading.png" alt="">
-    <div class="text-2xl text-white bg-blue-500 px-6 rounded-lg py-3 hover:bg-blue-600 transition-all duration-300">
-        <a href="{{ route('login')}}">
-            Login /
-        </a>
-        <a href="{{ route('typeuser.page') }}">
-            Cadastro
-        </a>
+            <!-- Conteúdo Principal -->
+            <main class="flex-1 p-4 text-center">
+                <!-- Cabeçalho e conteúdo principal -->
+                <div class="flex justify-between p-8 items-center">
+                    <img src="{{ asset('assets/images/logoLading.png') }}" alt="Logo" class="h-12">
+                    <button class="text-2xl text-white bg-blue-500 px-6 rounded-lg py-3 hover:bg-blue-600 transition-all duration-300">Login/Cadastro</button>
+                </div>
+
+                <div class="text-white grid items-center justify-items-center mt-20">
+                    <p class="text-6xl font-bold">Aprender nunca foi tão divertido</p>
+                    <p class="text-3xl mb-6">Gamifique seus estudos com desafios, rankings e recompensa.</p>
+                    <button class="bg-blue-500 hover:bg-blue-600 transition-all px-6 py-3 text-2xl rounded-lg duration-300">Comece agora mesmo</button>
+                </div>
+            </main>
+        </div>
     </div>
-  </div>
 
-  <div class="text-white grid items-center justify-items-center mt-20">
-    <p class="text-6xl font-bold">Aprender nunca foi tão divertido</p>
-    <p class="text-3xl mb-6">Gamifique seus estudos com desafios, rankings e recompensa.</p>
-    <button class="bg-blue-500 hover:bg-blue-600 transition-all px-6 py-3 text-2xl rounded-lg duration-300">Comece agora mesmo</button>
-  </div>
-
+    <!-- Footer -->
+    <x-footer />
 </body>
 
 </html>
